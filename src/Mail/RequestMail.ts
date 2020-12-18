aff1d9f6eb274b941492275b5a3433d9d5e79463
Edrson <ederson.ramirez87@gmail.com>
2020-12-18 00:59:34 -0600

    cancelacion mail

--- src/Mail/RequestMail.ts
+++
@@ -1,8 +0,0 @@
-import {Descripcion} from './Descripcion';
-export interface RequestMail{
-    tipo:number;
-    para:string;
-    articulos:Descripcion[];
-    totalCompra:number; 
-    productoAgotado:string
-}
