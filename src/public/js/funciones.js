be28654de4a035002a0766c9314b826d442324f1
Luiskr1993 <luiskr1993@gmial.com>
2020-12-20 06:41:57 -0600

    Se agregaron las conexiones entre frontend y los microservicios de login, registro, carga de productos, generación de tablas de automatizadas

--- src/public/js/funciones.js
+++ src/public/js/funciones.js
@@ -1,3 +1,5 @@
+
+//**************************************************LÓGICA DE FUNCIÓN PARA HACER LOGIN************************************************ */
 var boton = document.getElementById("Ingresar");
 
 boton.addEventListener("click", function () {
@@ -54,7 +56,7 @@ boton.addEventListener("click", function () {
     }
   }
 });
-
+//**************************************************LÓGICA DE FUNCIÓN DE REGISTRO DE USUARIOS************************************************ */
 var boton2 = document.getElementById("registrar");
 
 boton2.addEventListener("click", function () {
@@ -98,7 +100,7 @@ boton2.addEventListener("click", function () {
     alert("La password debe tener minimo 6 Caracteres");
     return false;
   } else {
-    alert("se puede registrar usuario");
+    //alert("se puede registrar usuario");
 
     var nombre1 = document.getElementById("completo").value;
     var direccion1 = document.getElementById("dir").value;
@@ -155,6 +157,78 @@ boton2.addEventListener("click", function () {
   }
 });
 
+//**************************************************LÓGICA DE FUNCIÓN PARA CARGAR PRODUCTOS*********************************************** */
+var botonCargarProductos = document.getElementById("botonDescargarProductos");
+
+botonCargarProductos.addEventListener("click", function () {
+  alert("entro a la funcion descargar productos.");
+  re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    //aqui se debe crear la lógica para consumir el webservice de login
+    var user = "4"
+
+      var listaProductos = {
+        id: 1,
+        id_cliente: 0,
+        producto:"empty",
+        cantidad: "1",
+        subtotal: "1"
+      };
+console.log(stringify(listaProductos));
+      var URL = "http://34.68.127.94/"+user;
+      axios
+        .get(URL, JSON.stringify(listaProductos))
+        .then((res) => {
+          console.log(stringify("Entro a verificar dentorde la twew"));
+
+          if (res.status == 200) {
+            console.log(JSON.stringify(res.data));
+            alert("Búsqueda Realizada: " + JSON.stringify(res.data));
+          } else {
+            alert("No se encontraron productos");
+          }
+          console.log(res);
+        })
+        .catch(function (err) {
+          console.log(err);
+          alert(err);
+        });
+});
+
+function cargarProductosEnPantalla(){
+
+  alert("entro a la funcion descargar productos.");
+  re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    //aqui se debe crear la lógica para consumir el webservice de login
+    var user = "7"
+
+      var listaProductos = {
+        id: 1,
+        id_cliente: 0,
+        producto:"empty",
+        cantidad: "1",
+        subtotal: "1"
+      };
+      console.log(JSON.stringify(listaProductos));
+      axios
+        .get("http://34.70.142.209/api/car/"+user)
+        .then((res) => {
+          if (res.status == 200) {
+            console.log(JSON.stringify(res.data));
+            alert("Búsqueda Realizada: " + JSON.stringify(res.data));
+            verProductosTienda(JSON.stringify(res.data));
+          } else {
+            alert("No se encontraron productos");
+          }
+          console.log(res);
+        })
+        .catch(function (err) {
+          console.log("Error en llamada al microservisios");
+          console.log(err);
+          alert(err);
+        });
+}
+
+//**************************************************BOTÓN PARA CALCULAR EL SUBTOTAL DE PRODUCTO************************************************ */
 var calcularSubtotal = document.getElementById("subTotalProducto");
 calcularSubtotal.addEventListener("keydown", function(){
 
@@ -180,10 +254,11 @@ function calcularSubTotal(){
     document.getElementById("subTotalProducto").value = subtotal;
 }
 
+//**************************************************AÑADIR PRODUCTO AL CARRITO DE COMPRAS************************************************ */
 var agrearProducto = document.getElementById("btnAñadeAlCarrito");
 
-boton2.addEventListener("click", function () {
-  //alert("entro a la funcion2");
+agrearProducto.addEventListener("click", function () {
+  alert("entro a la funcion de añadir al carrito");
   re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
   var cantidad = document.getElementById("cantidadProductos").value;
   if(cantidad <= 0){
@@ -194,39 +269,48 @@ boton2.addEventListener("click", function () {
     var nombre1 = document.getElementById("nombreProducto").value;
     var precio1 = document.getElementById("precioProducto").value;
     var cantidad1 = document.getElementById("cantidadProducto").value;
-    var usuario1 = "admin";
+    var usuario1 = "1";
 
     var producto = {
-      id_cliente: usuario1,
-      producto: nombre1,
-      cantidad: cantidad1,
-      subtotal: precio1
+      id_cliente: "",
+      producto: "",
+      cantidad: "",
+      subtotal: ""
     }
-    var usuario = {
-      nombre: nombre1,
-      correo: email1,
-      direccion: direccion1,
-      celular: tel1,
-      password: pw,
-      tipo: tipoRegistro1,
-    };
-    console.log(JSON.stringify(usuario));
+    if (nombre1.value == "" && precio1.value == "" && cantidad1 == "0" && usuario1 == ""){
+      //si los parámetros vienen vacíos se llena el json con información por defecto
+      producto.id_cliente = "0";
+      producto.producto = "empty";
+      producto.cantidad = "0";
+      producto.subtotal = "0"
+
+      //prueba para verificar si crea la tabla de información
+
+    }else{
+        //si los parámetros vienen con información, se asignan a los parámetros del json
+        producto.id_cliente = usuario1;
+        producto.producto = nombre1;
+        producto.cantidad =  cantidad1;
+        producto.subtotal = precio1;
+
+        //JSON construido para enviar 
+    console.log(JSON.stringify(producto));
     const headers = {
       "Content-Type": "application/json",
     };
     axios
-      .post("http://34.68.127.94/registro", JSON.stringify(usuario), {
+      .post("http://34.70.142.209/api/car", JSON.stringify(producto), {
         headers: headers,
       })
       .then((res) => {
-        console.log("Retorno: " + res);
-        if (res.status == 201) {
-          alert("Usuario registrado exitosamente!");
-          var url = "/";
-          $(location).attr("href", url);
+        console.log("Respuesta MS_Carrito: " + JSON.stringify(res.data));
+        if (res.status == 200) {
+          alert("Producto agregado a carrito exitosamente!");
+          //var url = "/";
+          //$(location).attr("href", url);
         } else {
-          document.getElementById("spanmensaje").innerText =
-            "No se pudo registrar el usuario, intente de nuevo.";
+          //document.getElementById("spanmensaje").innerText =
+            //"No se pudo registrar el usuario, intente de nuevo.";
           alert("No se pudo registrar el usuario, intente de nuevo.");
         }
       })
@@ -234,11 +318,235 @@ boton2.addEventListener("click", function () {
         alert("[ERROR]: " + err);
         console.log(err);
       });
+    }
 
 });
 
+function agregarProductoCarrito(){
+  re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+  var cantidad = document.getElementById("cantidadProducto").value;
+  if(cantidad <= 0){
+      alert("Debe elegir al menos un producto para agregar al carrito de compra.");
+  }else if(cantidad >0)
+    var nombre1 = document.getElementById("nombreProducto").value;
+    var precio1 = document.getElementById("precioProducto").value;
+    var cantidad1 = document.getElementById("cantidadProducto").value;
+    var usuario1 = "";
+
+    var producto = {
+      id_cliente: "",
+      producto: "",
+      cantidad: "",
+      subtotal: ""
+    }
+    if (nombre1.value == "" || precio1.value == "" || cantidad1 == "0" || usuario1 == ""){
+      //si los parámetros vienen vacíos se llena el json con información por defecto
+      console.log("El json no trae todos los campos")
+      producto.id_cliente = "0";
+      producto.producto = "empty";
+      producto.cantidad = "0";
+      producto.subtotal = "0"
+
+      alert("El producto no se ha podido almacenar.");
+      console.log("El producto no se ha podido almacenar.");
+
+    }else{
+        //si los parámetros vienen con información, se asignan a los parámetros del json
+        producto.id_cliente = usuario1;
+        producto.producto = nombre1;
+        producto.cantidad =  cantidad1;
+        producto.subtotal = precio1;
+
+        //JSON construido para enviar 
+    console.log(JSON.stringify(producto));
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    axios
+      .post("http://34.70.142.209/api/car", JSON.stringify(producto), {
+        headers: headers,
+      })
+      .then((res) => {
+        console.log("Respuesta MS_Carrito: " + JSON.stringify(res.data));
+        if (res.status == 200) {
+          alert("Producto agregado a carrito exitosamente!");
+          //var url = "/";
+          //$(location).attr("href", url);
+        } else {
+          //document.getElementById("spanmensaje").innerText =
+            //"No se pudo registrar el usuario, intente de nuevo.";
+          alert("No se pudo registrar el usuario, intente de nuevo.");
+        }
+      })
+      .catch(function (err) {
+        alert("[ERROR]: " + err);
+        console.log(err);
+      });
+    }
+}
+
+//**************************************************REDIRECCIÓN A CARRITO DE COMPRAS************************************************ */
 var carrito = document.getElementById("botonCarrito");
 
 carrito.addEventListener("click", function () {
   location("carrito.html");
 });
+
+
+//**************************************************FUNCIÓN PARA CONSTRUIR TABLA EN BASE A JSON************************************************ */
+// function obtieneDatos(data){
+//   console.log("Entro a la funcion de obtener datos");
+//   var tabla = document.createElement("table");
+//   var datos = JSON.parse(data);
+//   console.log(datos);
+
+
+//   for(let item of Object.keys(datos)){
+//     console.log(item +": "+ datos[item]);
+//     tabla.innerHTML += `
+//         <td>${datos[item]}</td>
+//     `
+//   }
+
+//   //console.log(tabla);
+//   return tabla;
+// }
+
+function crearTablaDesdeJson(data){
+  var datosJson = JSON.parse(data);
+  data = Object.keys(datosJson);
+  console.log(data);
+  console.log(data.length);
+  var table = document.createElement("table");
+
+  var thead = table.createTHead();
+  var tbody = table.createTBody();
+  console.log("Size de json: "+data.length)
+  var col = [];
+
+  for(var i = 0; i < data.length; i++){
+    console.log("Elemento_"+i+": "+data[i]);
+    var key = data[i];
+    if(col.indexOf(key) === -1){
+      col.push(key);
+    }
+  }
+
+  var cabecera = thead.insertRow(-1);
+  console.log("Tamaño de columnas: "+col.length);
+  for(var i = 0; i < col.length; i++){
+    var th = document.createElement("th");
+    th.innerHTML = col[i];
+    cabecera.appendChild(th);
+  }
+
+  tr = tbody.insertRow(-1);
+  var tabCell;
+
+  console.log(datosJson);
+  for(let item of Object.keys(datosJson)){
+    var tabCell = tr.insertCell(-1);
+    tabCell.innerHTML = datosJson[item];
+  }
+  return table;
+}
+
+//*************************************************OBTENER PRODUCTOS DE LA TIENDA*********************************************** */
+function verProductosTienda(data){
+  //console.log("Funcion para cargar productos en pantalla...");
+  var datos = JSON.parse(data);
+  var productos;
+  var atributos = [];
+  
+
+  for(let item of Object.keys(datos)){
+     var atr = Object.values(datos[item]);
+     //console.log("Item: "+ atr);
+     atributos.push(atr);
+  }
+
+  //console.log("Total items: "+ atributos.length);
+
+  for(var i=0; i<atributos.length; i++){
+    var par = atributos[i];
+    var id = par[0];
+    var id_cliente = par[1];
+    var producto = par[2];
+    var cantidad = par[3];
+    var subtotal = par[4];
+    console.log("id: " + id + "  id:cliente: " + id_cliente + "  producto: " + producto + "  cantidad:  "+ cantidad + "  subtotal: "+subtotal);
+    crearCasillaProducto(id, id_cliente, producto, cantidad, subtotal);
+  }
+  
+
+}
+//*************************************************CREAR TARJETA DE PRODUCTO************************************************** */
+function crearCasillaProducto(id, id_cliente, producto, cantidad, subtotal){
+  console.log("******************************************************");
+
+
+
+  //var productoData2 = Object.keys(productoData);
+  const contenedor = document.getElementById("catalogo");
+
+  const div = document.createElement("div");
+  div.className = "col-sm-3";
+  
+
+  const divCard = document.createElement("div");
+  divCard.className = "card";
+  
+  div.appendChild(divCard);
+
+  const divCardBody = document.createElement("div");
+  divCardBody.className = "card-body";
+  
+  divCard.appendChild(divCardBody);
+
+  const h5 = document.createElement("h5");
+  h5.className = "card-title text-center";
+  
+
+  const img = document.createElement("img");
+  img.id = "img_obs";
+  img.src = "/imagen/alcanciabuena.png";
+
+  h5.appendChild(img);
+
+  const p1 = document.createElement("p");
+  p1.className = "card-text";
+  
+
+  const labelProducto = document.createElement("label");
+  labelProducto.id = "labelProducto";
+  labelProducto.textContent = producto;
+  const br = document.createElement("br");
+  const labelPrecio = document.createElement("label");
+  labelPrecio.id = "labelPrecio";
+  labelPrecio.textContent = subtotal;
+
+  p1.appendChild(labelProducto);
+  p1.appendChild(br);
+  p1.appendChild(labelPrecio);
+
+
+  const p2 = document.createElement("p");
+  p2.className = "card-text text-right";
+  
+  divCardBody.appendChild(h5);
+  divCardBody.appendChild(p1);
+  divCardBody.appendChild(p2);
+
+  const btnAgregar = document.createElement("a");
+  btnAgregar.modal = "modal";
+  btnAgregar.data = "#ModalProductos";
+  btnAgregar.href = "#";
+  btnAgregar.className = "btn btn-primary";
+  btnAgregar.textContent = "Agregar al Carrito";
+
+
+  p2.appendChild(btnAgregar);
+
+  contenedor.appendChild(div);
+
+}
