5cd31756e686d5a6955fe2eaf541cb1c582cac50
Cristian <moino.cristian@gmail.com>
2020-12-20 17:02:35 -0600

    se agregan listas ventas, compra directa backend
    se agregan listas ventas, compra directa backend

--- server/routes/index.js
+++ server/routes/index.js
@@ -36,6 +36,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 exports.__esModule = true;
+var safe_buffer_1 = require("safe-buffer");
 var express = require('express');
 var db = require('../db/index');
 var axios = require('axios');
@@ -93,35 +94,37 @@ router.get('/listaProductos', function (req, res, next) { return __awaiter(void 
         }
     });
 }); });
-//-------------------detalles usuario-------------------------
-router.get('/getUsuario', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
+router.get('/listarProductosProveedor', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
     var results, e_3;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, db.one(req.body.codusuario)];
+                console.log("lista productos proveedor");
+                _a.label = 1;
             case 1:
+                _a.trys.push([1, 3, , 4]);
+                return [4 /*yield*/, db.allProductosProveedor(req.body.codproveedor)];
+            case 2:
                 results = _a.sent();
                 res.status(200).json(results);
-                return [3 /*break*/, 3];
-            case 2:
+                return [3 /*break*/, 4];
+            case 3:
                 e_3 = _a.sent();
                 console.log(e_3);
                 res.sendStatus(500);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
         }
     });
 }); });
-//----------------------------- borrar proveedor------------------
-router["delete"]('/:id', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
+//-------------------detalles usuario-------------------------
+router.get('/getUsuario', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
     var results, e_4;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, db.borrar(req.params.id)];
+                return [4 /*yield*/, db.one(req.body.codusuario)];
             case 1:
                 results = _a.sent();
                 res.status(200).json(results);
@@ -135,9 +138,30 @@ router["delete"]('/:id', function (req, res, next) { return __awaiter(void 0, vo
         }
     });
 }); });
+//----------------------------- borrar proveedor------------------
+router["delete"]('/:id', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
+    var results, e_5;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, db.borrar(req.params.id)];
+            case 1:
+                results = _a.sent();
+                res.status(200).json(results);
+                return [3 /*break*/, 3];
+            case 2:
+                e_5 = _a.sent();
+                console.log(e_5);
+                res.sendStatus(500);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
 //-------------------------- registrar usuario-------------------------
 router.post('/registro', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
-    var results, results, e_5;
+    var results, results, e_6;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
@@ -155,8 +179,8 @@ router.post('/registro', function (req, res, next) { return __awaiter(void 0, vo
                 _a.label = 4;
             case 4: return [3 /*break*/, 6];
             case 5:
-                e_5 = _a.sent();
-                console.log(e_5);
+                e_6 = _a.sent();
+                console.log(e_6);
                 res.sendStatus(500);
                 return [3 /*break*/, 6];
             case 6: return [2 /*return*/];
@@ -165,52 +189,35 @@ router.post('/registro', function (req, res, next) { return __awaiter(void 0, vo
 }); });
 //-------------------------CRUD PRODUCTO-------------------------------
 router.post('/insertarProducto', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var results, e_6;
+    var results, results2, e_7;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                _a.trys.push([0, 2, , 3]);
+                _a.trys.push([0, 3, , 4]);
                 return [4 /*yield*/, db.insertarProducto(req.body.nombre, req.body.cat_codcategoria, req.body.stock, req.body.precio, req.body.precio_venta)];
             case 1:
                 results = _a.sent();
-                res.status(200).json(results);
-                return [3 /*break*/, 3];
+                return [4 /*yield*/, db.insertarProductoProveedor(results, req.body.codproveedor)];
             case 2:
-                e_6 = _a.sent();
-                console.log(e_6);
+                results2 = _a.sent();
+                res.status(200).json(results);
+                return [3 /*break*/, 4];
+            case 3:
+                e_7 = _a.sent();
+                console.log(e_7);
                 res.sendStatus(500);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
+                return [3 /*break*/, 4];
+            case 4: return [2 /*return*/];
         }
     });
 }); });
 router.put('/modificarProducto/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var results, e_7;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, db.updateProducto(req.body.nombre, req.body.cat_codcategoria, req.body.stock, req.body.precio, req.body.precio_venta, req.params.id)];
-            case 1:
-                results = _a.sent();
-                res.status(200).json(results);
-                return [3 /*break*/, 3];
-            case 2:
-                e_7 = _a.sent();
-                console.log(e_7);
-                res.sendStatus(500);
-                return [3 /*break*/, 3];
-            case 3: return [2 /*return*/];
-        }
-    });
-}); });
-router["delete"]('/eliminarProducto/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
     var results, e_8;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
                 _a.trys.push([0, 2, , 3]);
-                return [4 /*yield*/, db.borrarProducto(req.params.id)];
+                return [4 /*yield*/, db.updateProducto(req.body.nombre, req.body.cat_codcategoria, req.body.stock, req.body.precio, req.body.precio_venta, req.params.id)];
             case 1:
                 results = _a.sent();
                 res.status(200).json(results);
@@ -224,17 +231,59 @@ router["delete"]('/eliminarProducto/:id', function (req, res) { return __awaiter
         }
     });
 }); });
+router["delete"]('/eliminarProducto/:id', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var results, e_9;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, db.borrarProducto(req.params.id)];
+            case 1:
+                results = _a.sent();
+                res.status(200).json(results);
+                return [3 /*break*/, 3];
+            case 2:
+                e_9 = _a.sent();
+                console.log(e_9);
+                res.sendStatus(500);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
 //-----------------------------------------------------------------
+router.get('/listarCompras', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
+    var results, e_10;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, db.listarCompras(req.body.codusuario)];
+            case 1:
+                results = _a.sent();
+                res.status(200).json(results);
+                return [3 /*break*/, 3];
+            case 2:
+                e_10 = _a.sent();
+                console.log(e_10);
+                res.sendStatus(500);
+                return [3 /*break*/, 3];
+            case 3: return [2 /*return*/];
+        }
+    });
+}); });
 router.post('/auth', function (request, response) {
-    var email = request.correo;
-    var password = request.password;
+    var email = request.body.correo;
+    var password = request.body.password;
     console.log("entro al auth " + email + " " + password);
     if (email && password) {
         con.query('SELECT * FROM usuario WHERE email = ? AND contra = ?', [email, password], function (error, results, fields) {
             if (results.length > 0) {
                 request.session.loggedin = true;
                 request.session.username = email;
-                response.status(200).send(JSON.stringify({ "codusuario": results[0].codusuario })); //REDIRECT 
+                var buf = safe_buffer_1.Buffer.from(results[0].tipo);
+                console.log(buf.toString());
+                response.status(200).send(JSON.stringify({ "codusuario": results[0].codusuario, "tipo": results[0].tipo })); //REDIRECT 
             }
             else {
                 response.send(401);
@@ -251,23 +300,55 @@ var header = {
     "Content-Type": "application/json"
 };
 //-------------------------------compra--------------------------
+router.post('/directa', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var producto, results, results2, e_11;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 4, , 5]);
+                return [4 /*yield*/, db.getProducto(req.body.idproducto)];
+            case 1:
+                producto = _a.sent();
+                console.log("esto trae el producto: ", producto);
+                return [4 /*yield*/, db.insertarCompra(req.body.idcliente, producto.subtotal)];
+            case 2:
+                results = _a.sent();
+                return [4 /*yield*/, db.insertarDetalleCompra(results, producto)];
+            case 3:
+                results2 = _a.sent();
+                res.status(200).json(results2);
+                return [3 /*break*/, 5];
+            case 4:
+                e_11 = _a.sent();
+                console.log(e_11);
+                res.sendStatus(500);
+                return [3 /*break*/, 5];
+            case 5: return [2 /*return*/];
+        }
+    });
+}); });
 router.post('/compra', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
-    var id;
+    var id, tot;
     return __generator(this, function (_a) {
         console.log("cod cliente" + req.body.idcliente);
         id = req.body.idcliente;
+        tot = 0;
         funcionLista(req, res, function () {
             return __awaiter(this, void 0, void 0, function () {
-                var idCompra, _a, _b, _i, i, iddetalle, e_9;
+                var j, idCompra, _a, _b, _i, i, iddetalle, e_12;
                 return __generator(this, function (_c) {
                     switch (_c.label) {
                         case 0:
                             _c.trys.push([0, 6, , 7]);
-                            return [4 /*yield*/, db.insertarCompra(id)];
-                        case 1:
-                            idCompra = _c.sent();
                             //res.json(idCompra);
                             console.log("tamaño ", objsCarrito.length);
+                            for (j in objsCarrito) {
+                                tot = tot + (objsCarrito[j].subtotal * objsCarrito[j].cantidad);
+                            }
+                            console.log("total: ", tot);
+                            return [4 /*yield*/, db.insertarCompra(id, tot)];
+                        case 1:
+                            idCompra = _c.sent();
                             _a = [];
                             for (_b in objsCarrito)
                                 _a.push(_b);
@@ -285,11 +366,11 @@ router.post('/compra', function (req, res, next) { return __awaiter(void 0, void
                             _i++;
                             return [3 /*break*/, 2];
                         case 5:
-                            res.json(objsCarrito);
+                            res.status(200);
                             return [3 /*break*/, 7];
                         case 6:
-                            e_9 = _c.sent();
-                            console.log(e_9);
+                            e_12 = _c.sent();
+                            console.log(e_12);
                             res.sendStatus(500);
                             return [3 /*break*/, 7];
                         case 7: return [2 /*return*/];
@@ -327,4 +408,66 @@ function llamarServicioListar(req, res, callback) {
         res.send(e.message);
     });
 }
+//-------------------------factura-----------------------------
+router.post('/factura', function (req, res, next) { return __awaiter(void 0, void 0, void 0, function () {
+    var idcompra, nit, dir, idcliente, tot;
+    return __generator(this, function (_a) {
+        console.log("cod compra" + req.body.idcompra);
+        idcompra = req.body.idcompra;
+        nit = req.body.nit;
+        dir = req.body.direccion;
+        idcliente = req.body.idcliente;
+        tot = 0;
+        funcionLista(req, res, function () {
+            return __awaiter(this, void 0, void 0, function () {
+                var detallecompra, j, idFactura, _a, _b, _i, i, iddetalle, e_13;
+                return __generator(this, function (_c) {
+                    switch (_c.label) {
+                        case 0:
+                            _c.trys.push([0, 7, , 8]);
+                            return [4 /*yield*/, db.allDetalleCompra(idcompra)];
+                        case 1:
+                            detallecompra = _c.sent();
+                            //res.json(idCompra);
+                            console.log("esto trae el detallecompra: ", detallecompra);
+                            console.log("tamaño ", detallecompra.length);
+                            for (j in detallecompra) {
+                                tot = tot + (detallecompra[j].subtotal * detallecompra[j].cantidad);
+                            }
+                            console.log("total: ", tot);
+                            return [4 /*yield*/, db.insertarFactura(idcliente, nit, dir, tot)];
+                        case 2:
+                            idFactura = _c.sent();
+                            _a = [];
+                            for (_b in detallecompra)
+                                _a.push(_b);
+                            _i = 0;
+                            _c.label = 3;
+                        case 3:
+                            if (!(_i < _a.length)) return [3 /*break*/, 6];
+                            i = _a[_i];
+                            console.log("for ", i, " ", detallecompra[i]);
+                            return [4 /*yield*/, db.insertarDetalleFactura(idFactura, detallecompra[i])];
+                        case 4:
+                            iddetalle = _c.sent();
+                            _c.label = 5;
+                        case 5:
+                            _i++;
+                            return [3 /*break*/, 3];
+                        case 6:
+                            res.json(detallecompra);
+                            return [3 /*break*/, 8];
+                        case 7:
+                            e_13 = _c.sent();
+                            console.log(e_13);
+                            res.sendStatus(500);
+                            return [3 /*break*/, 8];
+                        case 8: return [2 /*return*/];
+                    }
+                });
+            });
+        }.bind(this));
+        return [2 /*return*/];
+    });
+}); });
 module.exports = router;
